d98f9fe80819a0c85769a8be93e4c01d0267318d
Rizqi Bagus Andrean <bagus.ok55@gmail.com>
2025-05-10 19:56:23 +0700

    fix: Replace Inertia.delete with router.delete in handleDeleteUser function

--- resources/js/lib/userDelete.utils.ts
+++ resources/js/lib/userDelete.utils.ts
@@ -1,6 +1,6 @@
-import { Inertia } from "@inertiajs/react";
+import { router } from "@inertiajs/react";
 import { route } from "ziggy-js";
 
 export function handleDeleteUser(userId: number) {
-  Inertia.delete(route("users.destroy", userId));
+    router.delete(route("users.destroy", userId));
 }
