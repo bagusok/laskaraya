c62af8d27ce557ee7fa1e0cf26fb0ce11c39f116
Rizqi Bagus Andrean <bagus.ok55@gmail.com>
2025-05-21 08:57:37 +0700

    fix: Change column type from 'date' to 'year' in periods migration

--- database/migrations/2025_05_16_025311_periods.php
+++ database/migrations/2025_05_16_025311_periods.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('periods', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->date('year');
+            $table->year('year');
             $table->timestamps();
         });
     }
